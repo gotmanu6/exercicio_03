fecbd216ce96017672770387c3dd3215475652d7
Emanuelly <manu.presteslopes10@gmail.com>
2026-03-23 12:44:50 -0300

    model

---
+++ manu/src/main/java/models/PedidoModel.java
@@ -0,0 +1,49 @@
+package models;
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+@Entity
+@Table(name="TB_PEDIDO")
+public class PedidoModel {
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime data;
+    private BigDecimal valorTotal;
+    private String status;
+
+    public PedidoModel(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
+
